9cfabc6ef17547fee5585c474ca65338cc5c164a
Elena Kot <elenakot07302@gmail.com>
2021-09-23 17:28:13 -0400

    cart3

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
-const {
-  models: { Orders, Order_details },
-} = require("../db");
+
+const Product = require("../db/models/product");
+const Orders = require("../db/models/order");
+const Order_detail = require("../db/models/orderDetails");
 
 // POST /api/cart
 router.post("/", async (req, res, next) => {
   try {
-    const products = await Product.findAll();
+    res.status(201).send(await Order_detail.create(req.body));
+    //   const price = await Product.
+    // const products = await Product.findAll();
     // console.log("GET /PRODUCTS > ", products);
     // if (!products) {
     //     next({ status: 500, message: "Database query failed." });
     // }
-    res.json(products);
+    // res.json(products);
   } catch (err) {
     console.log(">  /api/products ERR: ", err);
     next(err);
