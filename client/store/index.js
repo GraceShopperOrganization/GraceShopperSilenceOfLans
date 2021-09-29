2a224117f9c863dba6b2491ec4a87b366f2fc7c5
Ross Kulyk <uaross@hotmail.com>
2021-09-28 22:51:15 -0400

    heroku issue

--- client/store/index.js
+++ client/store/index.js
@@ -3,22 +3,22 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import auth from "./auth";
-import userlist from "./userlist";
+import userlistReducer from "./userlist";
 import productsReducer from "./products";
 import productReducer from "./product";
 import usersReducer from "./users";
 import cartReducer from "./orders";
 
 const reducer = combineReducers({
-  auth,
-  productsReducer,
-  product: productReducer,
-  userlist,
-  usersReducer,
-  cart: cartReducer,
+    auth,
+    productsReducer,
+    product: productReducer,
+    userlist: userlistReducer,
+    usersReducer,
+    cart: cartReducer
 });
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+    applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
 const store = createStore(reducer, middleware);
 
